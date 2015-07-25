23f59da7b45ee899372437e9c990406c02513173
knuth87 <knuth.87@mail.ru>
2015-07-25 16:30:19 +0500

    Firsts

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('originStore', []);
+app.controller('OriginController', function($timeout) {
+  this.url = '';
+  this.compares = [];
+  this.AddCompare = function() {
+    var comp = {
+      url:'',
+      status:0
+    };
+    this.compares.push(comp);
+  };
+  this.DeleteCompare = function(ind) {
+    this.compares.splice(ind,1);
+  };
+  this.SubmitCompare = function() {
+    angular.forEach(this.compares, function(val,key) {
+      if(val.url != '') { 
+        val.status = 1;
+        $timeout( function(){
+          val.status = 2;
+        }, 5000);
+      }
+      else {
+        val.status = 0;
+      }
+    });
+  };
+});
